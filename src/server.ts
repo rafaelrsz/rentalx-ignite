bf7ff1c36731ed2469490fb705b3b039e57559a3
Rafael <rafa.rsouz@gmail.com>
2022-03-24 20:13:56 -0300

    Chapter 2.2

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
+app.use(express.json());
+app.use("/categories", categoriesRoutes);
+
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+  return response.json({ message: "hello" });
 });
 
 app.listen(3333, () => console.log("Server is running"));
